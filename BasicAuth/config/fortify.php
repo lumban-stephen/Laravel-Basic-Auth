3cd6a99e4800c7d7a14bf3c27e4b724da59aa448
stpn-lmbn <stephenlumban@gmail.com>
2021-10-01 22:30:52 +0800

    Update fortify.php
    remove unused features

--- BasicAuth/config/fortify.php
+++ BasicAuth/config/fortify.php
@@ -134,12 +134,7 @@ return [
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
-        // Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirmPassword' => true,
-        ]),
+        Features::emailVerification()
     ],
 
 ];
